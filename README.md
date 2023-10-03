373eb9c8792271641534caaf2ffe1813d01e7a45
lerkooow <lerkoooow@gmail.com>
2023-10-03 21:32:53 +0700

    9.9: Patientor backend, step2

--- README.md
+++ README.md
@@ -12,7 +12,8 @@
 
 ## Typing an Express app
 
-9.8: Patientor backend, step1
+9.8: Patientor backend, step1  
+9.9: Patientor backend, step2  
 
 
 
--- patientor/build/index.js
+++ patientor/build/index.js
@@ -5,12 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+
+app.use((_req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(express_1.default.json());
-const PORT = 3000;
-app.get('/ping', (_req, res) => {
+
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
 });
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
